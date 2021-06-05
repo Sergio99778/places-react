e9e6001c02c855ad3509fffd265f17a93dea4ca5
Sergio99778 <sergio4brandon@gmail.com>
2021-06-05 14:57:31 -0500

    state de un componente.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title.js'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <section>
-      <div>
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      numero: 0
+    };
+
+    setInterval(()=>{
+      this.setState({
+        numero: this.state.numero + 1
+      })
+    }, 1000)
+  }
+
+  render() {
+    return (
+      <section>
         <div>
-          <Title title="Places"></Title>
-          <button>Crear cuenta gratuita</button>
-          <img src={process.env.PUBLIC_URL + '/images/slondotpics.jpg' } alt="" height="300"/>
           <div>
-            <ul>
-              <li>
-                <h3>Calificaciones con emociones</h3>
-                <p>Califica tus lugares con experiencias, no con numeros</p>
-              </li>
-              <li>
-                <h3>¿Sin internet? Sin problemas</h3>
-                <p>Places funciona sin internet o en conexiones lentas</p>
-              </li>
-              <li>
-                <h3>Tus lugares favoritos</h3>
-                <p>Define la lista de tus lugares favoritos</p>
-              </li>
-            </ul>
+            <Title title="Places"></Title>
+            <h2>{this.state.numero}</h2>
+            <button>Crear cuenta gratuita</button>
+            <img src={process.env.PUBLIC_URL + '/images/slondotpics.jpg' } alt="" height="300"/>
+            <div>
+              <ul>
+                <li>
+                  <h3>Calificaciones con emociones</h3>
+                  <p>Califica tus lugares con experiencias, no con numeros</p>
+                </li>
+                <li>
+                  <h3>¿Sin internet? Sin problemas</h3>
+                  <p>Places funciona sin internet o en conexiones lentas</p>
+                </li>
+                <li>
+                  <h3>Tus lugares favoritos</h3>
+                  <p>Define la lista de tus lugares favoritos</p>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
+  
 }
 
 export default App;
