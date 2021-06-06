b2d92aa8f749290416aad1b7bd1b6733ede77748
Sergio99778 <sergio4brandon@gmail.com>
2021-06-05 20:16:19 -0500

    Colores material design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton'
 
+import {indigo400} from 'material-ui/styles/colors'
+
 import Title from './components/Title.js'
 
 import './App.css';
@@ -17,23 +19,23 @@ class App extends Component {
               <RaisedButton label="Crear cuenta gratuita" secondary={true}></RaisedButton>
               <img className="Header-illustration" src={process.env.PUBLIC_URL + '/images/slondotpics.jpg' } alt=""/>
             </div>
-            <div>
-              <ul>
-                <li>
-                  <h3>Calificaciones con emociones</h3>
-                  <p>Califica tus lugares con experiencias, no con numeros</p>
-                </li>
-                <li>
-                  <h3>¿Sin internet? Sin problemas</h3>
-                  <p>Places funciona sin internet o en conexiones lentas</p>
-                </li>
-                <li>
-                  <h3>Tus lugares favoritos</h3>
-                  <p>Define la lista de tus lugares favoritos</p>
-                </li>
-              </ul>
-            </div>
           </div>
+        </div>
+        <div style={{'background-color': indigo400,'padding':'50px'}}>
+          <ul>
+            <li>
+              <h3>Calificaciones con emociones</h3>
+              <p>Califica tus lugares con experiencias, no con numeros</p>
+            </li>
+            <li>
+              <h3>¿Sin internet? Sin problemas</h3>
+              <p>Places funciona sin internet o en conexiones lentas</p>
+            </li>
+            <li>
+              <h3>Tus lugares favoritos</h3>
+              <p>Define la lista de tus lugares favoritos</p>
+            </li>
+          </ul>
         </div>
       </MuiThemeProvider>
     );
