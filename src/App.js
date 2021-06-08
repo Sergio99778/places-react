a3a1afbee511273fa25a7ed44c177ca246f796c1
Sergio99778 <sergio4brandon@gmail.com>
2021-06-07 22:51:00 -0500

    Hijo unico para MuiThemeProvider.

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ class App extends Component {
   }
 
   places() {
-    return data.places.map(place => {
+    return data.places.map((place,index) => {
       return(
-        <div className="col-xs-12 col-sm-4">
+        <div className="col-xs-12 col-sm-4" key={index}>
           <Card>
             <CardMedia>
               <img src={process.env.PUBLIC_URL + place.imageURL} alt="" />
@@ -37,64 +37,66 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="Header-background">
-          <div style={{"width":"80%", "margin": "0 auto"}}>
-            <div className="Header-main">
-              <Title title="Places"></Title>
-              <RaisedButton label="Crear cuenta gratuita" secondary={true}></RaisedButton>
-              <img className="Header-illustration" src={process.env.PUBLIC_URL + '/images/slondotpics.jpg' } alt=""/>
-            </div>
-            <div>
-            <ul>
-            <Card className="Header-Benefit">
-              <CardText >
-                <div className="row">
-                  <div className="Header-Benefit-image col-xs" style={{'backgroundColor':redA400}}>
-                    <img style={{'width': '100%'}} src={process.env.PUBLIC_URL + '/images/heart.png'} alt="" />
+        <section>
+          <div className="Header-background">
+            <div style={{"width":"80%", "margin": "0 auto"}}>
+              <div className="Header-main">
+                <Title title="Places"></Title>
+                <RaisedButton label="Crear cuenta gratuita" secondary={true}></RaisedButton>
+                <img className="Header-illustration" src={process.env.PUBLIC_URL + '/images/slondotpics.jpg' } alt=""/>
+              </div>
+              <div>
+              <ul>
+              <Card className="Header-Benefit">
+                <CardText >
+                  <div className="row">
+                    <div className="Header-Benefit-image col-xs" style={{'backgroundColor':redA400}}>
+                      <img style={{'width': '100%'}} src={process.env.PUBLIC_URL + '/images/heart.png'} alt="" />
+                    </div>
+                    <div className="Header-Benefit-content col-xs">
+                      <h3>Califica con el corazón</h3>
+                      <p>Califica tus lugares con experiencias, no con numeros</p>
+                    </div>
                   </div>
-                  <div className="Header-Benefit-content col-xs">
-                    <h3>Califica con el corazón</h3>
-                    <p>Califica tus lugares con experiencias, no con numeros</p>
+                </CardText>
+              </Card>
+              <Card className="Header-Benefit">
+                <CardText >
+                  <div className="row">
+                    <div className="Header-Benefit-image col-xs" style={{'backgroundColor':lightBlueA400}}>
+                      <img style={{'width': '100%'}} src={process.env.PUBLIC_URL + '/images/no-internet.png'} alt="" />
+                    </div>
+                    <div className="Header-Benefit-content col-xs">
+                      <h3>¿Sin internet? Sin problemas</h3>
+                      <p>Places funciona sin internet o en conexiones lentas</p>
+                    </div>
                   </div>
-                </div>
-              </CardText>
-            </Card>
-            <Card className="Header-Benefit">
-              <CardText >
-                <div className="row">
-                  <div className="Header-Benefit-image col-xs" style={{'backgroundColor':lightBlueA400}}>
-                    <img style={{'width': '100%'}} src={process.env.PUBLIC_URL + '/images/no-internet.png'} alt="" />
+                </CardText>
+              </Card>
+              <Card className="Header-Benefit">
+                <CardText  >
+                  <div className="row">
+                    <div className="Header-Benefit-image col-xs" style={{'backgroundColor':amberA400}} >
+                      <img style={{'width': '100%', 'backgroundColor':amberA400}} src={process.env.PUBLIC_URL + '/images/favorite.png'} alt="" />
+                    </div>
+                    <div className="Header-Benefit-content col-xs">
+                      <h3>Añade tus lugares favoritos</h3>
+                      <p>Define la lista de tus favoritos en el mundo</p>
+                    </div>
                   </div>
-                  <div className="Header-Benefit-content col-xs">
-                    <h3>¿Sin internet? Sin problemas</h3>
-                    <p>Places funciona sin internet o en conexiones lentas</p>
-                  </div>
-                </div>
-              </CardText>
-            </Card>
-            <Card className="Header-Benefit">
-              <CardText  >
-                <div className="row">
-                  <div className="Header-Benefit-image col-xs" style={{'backgroundColor':amberA400}} >
-                    <img style={{'width': '100%'}} src={process.env.PUBLIC_URL + '/images/favorite.png'} alt="" />
-                  </div>
-                  <div className="Header-Benefit-content col-xs">
-                    <h3>Añade tus lugares favoritos</h3>
-                    <p>Define la lista de tus lugares favoritos en el mundo</p>
-                  </div>
-                </div>
-              </CardText>
-            </Card>
-          </ul>
+                </CardText>
+              </Card>
+            </ul>
+              </div>
             </div>
           </div>
-        </div>
-        <div style={{'backgroundColor': indigo400,'padding':'50px', 'color': 'white'}}>
-          <h3 style={{'fontSize':'24px'}}>Sitios populares</h3>
-          <div className="row">
-            {this.places()}
+          <div style={{'backgroundColor': indigo400,'padding':'50px', 'color': 'white'}}>
+            <h3 style={{'fontSize':'24px'}}>Sitios populares</h3>
+            <div className="row">
+              {this.places()}
+            </div>
           </div>
-        </div>
+        </section>
       </MuiThemeProvider>
     );
   }
